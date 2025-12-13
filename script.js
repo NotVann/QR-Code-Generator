d79d3dca97b0612eb874ae79fefa86a85d3cd9c3
NotVann <130763613+NotVann@users.noreply.github.com>
2025-12-13 08:36:21 +0700

    Add script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+let btn = document.querySelector(".qr-button");
+let qr_code_element = document.querySelector(".qr-code");
+let downloadLink = document.getElementById("downloadLink");
+
+btn.addEventListener("click", () => {
+  let user_input = document.querySelector("#input_text");
+
+  if (user_input.value !== "") {
+    if (qr_code_element.childElementCount === 0) {
+      generate(user_input);
+    } else {
+      qr_code_element.innerHTML = "";
+      generate(user_input);
+    }
+  } else {
+    console.log("Input tidak valid");
+    qr_code_element.style.display = "none";
+    downloadLink.style.display = "none";
+  }
+});
+
+function generateTimeCode() {
+  const now = new Date();
+
+  const hh = String(now.getHours()).padStart(2, "0");
+  const mm = String(now.getMinutes()).padStart(2, "0");
+  const ss = String(now.getSeconds()).padStart(2, "0");
+  const cs = String(Math.floor(now.getMilliseconds() / 10)).padStart(2, "0");
+
+  return hh + mm + ss + cs;
+}
+
+async function generate(user_input) {
+  qr_code_element.style.display = "";
+  qr_code_element.style.marginTop = "20px";
+  qr_code_element.style.marginBottom = "20px";
+
+  new QRCode(qr_code_element, {
+    text: `${user_input.value}`,
+    width: 180,
+    height: 180,
+    colorDark: "#000000",
+    colorLight: "#FFFFFF",
+    correctLevel: QRCode.CorrectLevel.M,
+  });
+
+  let timeCode = generateTimeCode();
+
+  setTimeout(() => {
+    let qr_code_canvas = document.querySelector("canvas");
+    let paddedCanvas = addPaddingToQRCode(qr_code_canvas, 20);
+
+    downloadLink.style.display = "block";
+    downloadLink.href = paddedCanvas.toDataURL("image/png");
+    downloadLink.download = `QR_${timeCode}.png`;
+
+  }, 300);
+}
+
+function addPaddingToQRCode(originalCanvas, padding) {
+  let canvas = document.createElement("canvas");
+  canvas.width = originalCanvas.width + padding * 2;
+  canvas.height = originalCanvas.height + padding * 2;
+
+  let context = canvas.getContext("2d");
+  context.fillStyle = "#FFFFFF";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(originalCanvas, padding, padding);
+
+  return canvas;
+}
